6d244a6adeb2efce9e2ff32d94ec8af6bd8d83db
Elbay Malik <eleo.666@gmail.com>
2022-02-08 14:57:19 +0100

    Finished the project

--- app/src/main/java/eu/eleo/affirmationphotos/adapter/ItemAdapter.kt
+++ app/src/main/java/eu/eleo/affirmationphotos/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import eu.eleo.affirmationphotos.R
@@ -16,6 +17,7 @@ class ItemAdapter(
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun getItemCount() = dataset.size
@@ -30,5 +32,6 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
