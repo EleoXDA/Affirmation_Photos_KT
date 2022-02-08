bc60bfd8da4fc993a1560a6de96dd36cce369162
Elbay Malik <eleo.666@gmail.com>
2022-02-08 14:41:06 +0100

    Affirmation image resources added

--- app/src/main/java/eu/eleo/affirmationphotos/model/Affirmation.kt
+++ app/src/main/java/eu/eleo/affirmationphotos/model/Affirmation.kt
@@ -1,5 +1,8 @@
 package eu.eleo.affirmationphotos.model
 
-data class Affirmation (val stringResourceId: Int){
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int){
 
 }
