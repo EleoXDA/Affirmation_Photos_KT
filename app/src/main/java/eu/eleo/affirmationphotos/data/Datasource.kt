9e7fe59bd51be6e6f2dd091c287305d34c5e7b70
Elbay Malik <eleo.666@gmail.com>
2022-02-08 14:47:13 +0100

    Affirmation image resources added in Datasource.kt

--- app/src/main/java/eu/eleo/affirmationphotos/data/Datasource.kt
+++ app/src/main/java/eu/eleo/affirmationphotos/data/Datasource.kt
@@ -6,16 +6,16 @@ import eu.eleo.affirmationphotos.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation> {
         return listOf(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation5, R.drawable.image5),
+            Affirmation(R.string.affirmation6, R.drawable.image6),
+            Affirmation(R.string.affirmation7, R.drawable.image7),
+            Affirmation(R.string.affirmation8, R.drawable.image8),
+            Affirmation(R.string.affirmation9, R.drawable.image9),
+            Affirmation(R.string.affirmation10, R.drawable.image10)
         )
     }
 }
