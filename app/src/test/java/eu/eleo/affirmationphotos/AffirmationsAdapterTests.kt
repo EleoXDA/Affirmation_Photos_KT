d9889d453f52dad614a2199efa0ff0badf4723ef
Elbay Malik <eleo.666@gmail.com>
2022-02-10 14:26:23 +0100

    Tester updates and fixes

--- app/src/test/java/eu/eleo/affirmationphotos/AffirmationsAdapterTests.kt
+++ app/src/test/java/eu/eleo/affirmationphotos/AffirmationsAdapterTests.kt
@@ -1,18 +1,23 @@
 package eu.eleo.affirmationphotos
 
 import android.content.Context
+import eu.eleo.affirmationphotos.adapter.ItemAdapter
 import eu.eleo.affirmationphotos.model.Affirmation
+import junit.framework.Assert.assertEquals
 import org.mockito.Mockito.mock
 import org.junit.Test
 
 class AffirmationsAdapterTests {
     private val context = mock(Context::class.java)
     @Test
-    fun adapter_size(){
+    fun adapter_size() {
         val data = listOf(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2)
         )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
     }
+
 
 }
